c34dcc3574a7ca415e32dce3813e94040f74ec59
master <master@SMLAB306PC04_25>
2025-04-24 21:46:30 -0500

    Agregar archivos de proyecto.

---
+++ Views/vDos.xaml.cs
@@ -0,0 +1,14 @@
+namespace agalloT3.Views;
+
+public partial class vDos : ContentPage
+{
+	public vDos()
+	{
+		InitializeComponent();
+	}
+
+    private void btnExportar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ Views/vUno.xaml.cs
@@ -0,0 +1,19 @@
+namespace agalloT3.Views;
+
+public partial class vUno : ContentPage
+{
+	public vUno()
+	{
+		InitializeComponent();
+	}
+
+    private void btnRegistro_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnGuardar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
