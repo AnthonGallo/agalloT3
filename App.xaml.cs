9ccf2d9994806ab32e2f9d5749744cbe1d9ad2b5
MIPC <anthongallo2003@gmail.com>
2025-05-05 19:41:36 -0500

    Act

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,11 +5,8 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-
-        // Establecer tema claro
         UserAppTheme = AppTheme.Light;
 
-        // Configurar la página principal con navegación
         MainPage = new NavigationPage(new Views.vUno())
         {
             BarBackgroundColor = Color.FromArgb("#5D4037"),
